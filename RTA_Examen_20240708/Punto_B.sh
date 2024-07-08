cd094cbe36fdfbed17428fa8b7a1a000871fe639
seek-coder <catrielgatto@gmail.com>
2024-07-08 20:12:20 -0300

    sh del punto B

---
+++ RTA_Examen_20240708/Punto_B.sh
@@ -0,0 +1,18 @@
+   98   ----- PUNTO B ----- 
+   99  cd /usr/local/bin/
+  100  touch Gatto_check_URL.sh
+  101  sudo touch Gatto_check_URL.sh
+  102  sudo vim Gatto_check_URL.sh
+  103  cat /home/bgatto/UTN-FRA_SO_Examenes/202406_Recu/bash_script/Lista_URL.txt 
+  104  cat /home/bgatto/UTN-FRA_SO_Examenes/202406_Recu/bash_script/check_URL.sh 
+  105  sudo vim Gatto_check_URL.sh
+  106  git add Gatto_check_URL.sh 
+  107  cat Gatto_check_URL.sh
+  108  sudo chmod +x Gatto_check_URL.sh 
+  109  ./Gatto_check_URL.sh 
+  110  cat Gatto_check_URL.sh 
+  111  sudo vim Gatto_check_URL.sh
+  112  ./Gatto_check_URL.sh 
+  113  ls -l /home/bgatto/UTN-FRA_SO_Examenes/202406_Recu/bash_script/
+  114  sudo vim Gatto_check_URL.sh
+  115  ./Gatto_check_URL.sh 
